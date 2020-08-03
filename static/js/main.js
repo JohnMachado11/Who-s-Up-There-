eeba3ef51907750e8945a8d80f70fbc4f787b2e8
John Machado <iamjohnmachado@gmail.com>
2020-08-02 22:24:03 -0500

    Version 1.0 8/2/20

---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+const apiData ={
+    url: 'http://api.open-notify.org/astros.json'
+}
+
+const {url} = apiData
+const apiUrl = `${url}`
+
+fetch(apiUrl)
+    .then( (data) => data.json() )
+    .then ( (astronauts) => createHtml(astronauts) )
+
+const createHtml = (data) => {
+    const link = "https://www.google.com/search?q=" 
+    console.log(data)
+    const html = `
+    <div> 
+        Total People In Space: ${data.number} <br>
+        ${data.people.map(x => `<div> Name: ${x.name}, Craft: ${x.craft} </div>`).join("")}
+    </div>
+    `
+    const astronautDiv = document.querySelector('.astronaut')
+    astronautDiv.innerHTML = html
+}
+
+// data.people.map(x => `Name: ${x.name}, Craft: ${x.craft}`).join("<br />")}
+
+// `
+// // <div class="name"> Name: ${data.people[0].name}</div>
+// // <div class="name"> Craft: ${data.people[0].craft}</div>
+// // `
