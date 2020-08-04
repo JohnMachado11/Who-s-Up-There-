5d26001c3238e1e26f87991b7baa7478a628fe6f
John Machado <iamjohnmachado@gmail.com>
2020-08-03 22:14:02 -0500

    V2.0 - 8/3/2020

--- static/js/main.js
+++ static/js/main.js
@@ -9,22 +9,27 @@ fetch(apiUrl)
     .then( (data) => data.json() )
     .then ( (astronauts) => createHtml(astronauts) )
 
+
 const createHtml = (data) => {
-    const link = "https://www.google.com/search?q=" 
-    console.log(data)
+    let googleLink = "https://www.google.com/search?q=" 
+    let launchLink = "https://www.nasa.gov/launchschedule/"
     const html = `
-    <div> 
-        Total People In Space: ${data.number} <br>
-        ${data.people.map(x => `<div> Name: ${x.name}, Craft: ${x.craft} </div>`).join("")}
-    </div>
-    `
+                <div> 
+                    <h2 style="box-shadow: 0 0 3px white;">WHO'S UP THERE?</h2> 
+                        <img src="/static/assets/earth.png" alt="earth" class="responsive">
+                    <h2>People in Space: ${data.number}</h2><hr>
+                        <a href="${launchLink}" style="color: inherit;">Launch Schedule</a> <hr>
+                    <p style="font-size: 17px">Names of Astronauts and Spacecraft They Currently Occupy</p>
+                    ${data.people.map(x => `
+                    <div> <br>
+                        <img src="/static/assets/astronaut.png" alt="astronaut" class="responsive" style="padding-right: 10px;">: 
+                        <a href="${googleLink} + ${x.name}+astronaut " style="color: inherit;">${x.name}</a> <br>
+                        <img src="/static/assets/rocket.png" alt="rocket" class="responsive" style="padding-right: 7px;">: 
+                        <a href="${googleLink} + ${x.craft}+spacecraft" style="color: inherit;">${x.craft}</a>
+                    </div>`).join("")}
+                </div>
+                `
     const astronautDiv = document.querySelector('.astronaut')
     astronautDiv.innerHTML = html
 }
 
-// data.people.map(x => `Name: ${x.name}, Craft: ${x.craft}`).join("<br />")}
-
-// `
-// // <div class="name"> Name: ${data.people[0].name}</div>
-// // <div class="name"> Craft: ${data.people[0].craft}</div>
-// // `
